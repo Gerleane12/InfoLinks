6ea3426bae37bbc540a5aabaab8e231c84a919ac
Gerleane <gerle.araujo@gmail.com>
2025-04-04 20:51:06 -0300

    primeiro commit

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import { View, Text} from "react-native";
+
+export default function Index() {
+    return (
+        <View>
+            <Text> Eu sou milior!!</Text>
+        </View>
+    );
+
+ }
+
+ 
