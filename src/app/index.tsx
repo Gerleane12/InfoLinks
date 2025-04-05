e6929f7e60e93e137801fcc796059f0da34b791a
Gerleane <gerle.araujo@gmail.com>
2025-04-05 17:23:35 -0300

    atualização

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,59 @@
-import { View, Text} from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+
 // Export default é o metodo usado para dizer ao ExporRouter que esse componente é uma Rota.
 export default function Index() {
+
     return (
         // Componente Text sendo usando para Renderizar um texto na tela.
         <View>
-            <Text> Eu sou Millior !!!</Text>
-            <Text> Gerleane</Text>
-            <Text> 28 anos</Text>
-            <Text>A vida é um processo!</Text>
-            <Text> Estudante de Informática</Text>
-            <Text>Futuro prospero!!</Text>
-            <Text> espero anciosa pela apresentação do PCCT!!</Text>
+
+            <Text style={styles.title}> Eu sou Millior !!!</Text>
+            <Text style={styles.title2}> Gerleane</Text>
+            <Text style={styles.title3}> 28 anos</Text>
+            <Text style={styles.title4}>A vida é um processo!</Text>
+            <Text style={styles.title5}> Estudante de Informática</Text>
+            <Text style={styles.title6}>Futuro prospero!!</Text>
+            <Text style={styles.title7}> espero anciosa pela apresentação do PCCT!!</Text>
         </View>
+
+        
     );
 
 }
     
+const styles = StyleSheet.create({
+    title: {
+        color: "red",
+        fontSize: 22,
+    },
+    
+    title2:{
+        color:"blue",
+        fontSize:22,
+    }, 
+title3:{
+    color:"pink",
+    fontSize:22,
+},
+title4:{
+    color:"red",
+    fontSize:24,
+},
+title5:{
+    color:"yellow",
+    fontSize:22,
+
+},
+title6:{
+    color:"black",
+    fontSize:22,
+},
+title7:{
+    color: "orange",
+    fontSize:22,
+},
+})
+
 
 
  
