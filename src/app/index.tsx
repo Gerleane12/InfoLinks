9b20f1a23fc9fb44a60763bd53712e57a381b567
Gerleane <gerle.araujo@gmail.com>
2025-04-05 15:22:24 -0300

    inserindo novos elementos

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,12 +1,17 @@
 import { View, Text} from "react-native";
-
+// Export default é o metodo usado para dizer ao ExporRouter que esse componente é uma Rota.
 export default function Index() {
     return (
+        // Componente Text sendo usando para Renderizar um texto na tela.
         <View>
-            <Text> Eu sou milior!!</Text>
+            <Text> Eu sou Millior !!!</Text>
+            <Text> Gerleane</Text>
+            <Text> 28 anos</Text>
         </View>
     );
 
- }
+}
+    
+
 
  
